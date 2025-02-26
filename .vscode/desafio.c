b8c4bbfd21bc4b12648cf1f62c5aa32d88835580
ViniciusSantos00 <santosvinii52@icloud.com>
2025-02-26 00:39:21 +0000

    add inf

--- .vscode/desafio.c
+++ .vscode/desafio.c
@@ -3,7 +3,19 @@
     int main (){
         printf("cadastro da carta!\n");
 
+        int populacao, turísmo;
+        float pib, areakm2;
+        char estado, cidade, cod;
         
+        printf("digite a populacao: \n");
+        scanf 
+
+        printf ("  ")
+        scnaf ("  ")
+
+    
+
+
         
 
 
